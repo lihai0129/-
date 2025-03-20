e495bbde731466903daed7efe9772327c81c838f
黎 <lihai_0129@qq.com>
2025-03-21 00:05:41 +0800

    Add files via upload

---
+++ 练习1.py
@@ -0,0 +1,26 @@
+mane="小明"
+age="28"
+print(f"{mane}今年{age}")
+
+name_is="Python"
+hool="is good  "
+hoos=(f"{name_is}{hool}")
+print(hoos)
+hoos=hoos.rstrip()
+print(hoos)
+
+name="Eric"
+name_is="Helleo"
+name_are="would you like to learn some Python tody ?"
+print(f"{name_is} {name},{name_are}")
+
+name_name="sUerc urchi"
+name_name=name_name.upper()
+print(name_name)
+name_name=name_name.lower()
+print(name_name)
+name_name=name_name.title()
+print(name_name)
+
+print('Albert Einstrin once said,"A person who never made a mistake never tried anything new",')
+
