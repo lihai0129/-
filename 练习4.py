bd35bb2835b4c7ec45c4b05d2dc0288eaea23149
黎 <lihai_0129@qq.com>
2025-03-24 21:48:31 +0800

    Add files via upload
    第四天练习 for、列表

---
+++ 练习4.py
@@ -0,0 +1,72 @@
+name=['alice','david','carolina']
+for name in name:
+    print(f"Hoole,I'm{name} How are you?")
+    print(f"Hoole,I'm{name} am you? is good\n")
+print("你好 拜拜")
+
+marking=list(range(1,5))
+print(marking)
+
+erver_marking=list(range(1,12,2))
+print(erver_marking)
+
+pizzes=['banned','ice-cong','pepperomi']
+for pizzes in pizzes:
+    print(f"I like {pizzes} pizza!")
+print('I really love pizza')
+
+zoo=['god is god','thione is good','mother is like banner']
+for zoo in zoo:
+    print(zoo)
+
+squares = []
+for value in range(1,11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
+
+# 上面简介版
+squaese= []
+for i in range(1,11):
+    squaese.append(i**2)
+
+print(squaese)
+
+dig=list(range(1,12))
+digs=sum(dig)
+print(min(dig))
+print(max(dig))
+print(sum(dig))
+print(digs)
+
+for i in range(1,21):
+    print(i)
+
+shus=list(range(1,1000001))
+print(shus)
+print(min(shus))
+print(max(shus))
+print(sum(shus))
+
+jishu=list(range(1,21,2))
+print(jishu)
+
+zhi=[]
+for beishus in range(3,31):
+    if beishus % 3 == 0:
+        zhi.append(beishus)
+print(zhi)
+
+
+lifangs=[]
+for lifang in range(1,11):
+    lifang**=3
+    lifangs.append(lifang)
+print(lifangs)
+
+lifanes=[lifan**3 for lifan in range(1,11)]
+print(lifanes)
+
+#切片
+ 
